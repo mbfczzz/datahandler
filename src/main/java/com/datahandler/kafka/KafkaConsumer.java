242ca16faa3c3170785f050a6823245824df0610
mbfczzzz <a317242392@163.com>
2025-09-06 12:38:33 +0800

    提交

--- src/main/java/com/datahandler/kafka/KafkaConsumer.java
+++ src/main/java/com/datahandler/kafka/KafkaConsumer.java
@@ -2,7 +2,9 @@ package com.datahandler.kafka;
 
 import com.alibaba.fastjson.JSONObject;
 import com.datahandler.entity.TiktokAnchorInfo;
+import com.datahandler.entity.TiktokRoomInfo;
 import com.datahandler.mapper.TiktokAnchorInfoMapper;
+import com.datahandler.mapper.TiktokRoomInfoMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -14,6 +16,9 @@ public class KafkaConsumer {
 
     @Autowired
     private TiktokAnchorInfoMapper tiktokAnchorInfoMapper;
+
+    @Autowired
+    private TiktokRoomInfoMapper tiktokRoomInfoMapper;
 
     // 消费单个消息
     @KafkaListener(topics = "anchor", groupId = "anchor-consumer-1")
@@ -29,8 +34,12 @@ public class KafkaConsumer {
     // 消费单个消息
     @KafkaListener(topics = "room",  groupId = "room-consumer-1")
     public void consumeMessageRoom(String message) {
+        log.info("收到消息: topic=room, message={}", message);
         System.out.println("Received message: " + message);
         // 处理业务逻辑
+        TiktokRoomInfo tiktokRoomInfo = JSONObject.parseObject(message, TiktokRoomInfo.class);
+        tiktokRoomInfoMapper.insert(tiktokRoomInfo);
+        log.info("消息成功: topic=room, message={}", message);
     }
 
 }
--- src/main/java/com/datahandler/mapper/TiktokRoomInfoMapper.java
+++ src/main/java/com/datahandler/mapper/TiktokRoomInfoMapper.java
@@ -1,10 +1,12 @@
 package com.datahandler.mapper;
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.datahandler.entity.TiktokRoomInfo;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
 @Mapper
 @Repository
-public interface TiktokRoomInfoMapper {
+public interface TiktokRoomInfoMapper extends BaseMapper<TiktokRoomInfo> {
 
 }
